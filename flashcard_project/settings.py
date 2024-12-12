6ed82c48f4a28cd1482290fbef2fa606ef591c4f
arnaugiribet <arnau.giribet@sanofi.com>
2024-12-12 11:51:37 +0000

    postgresql settings

--- flashcard_project/settings.py
+++ flashcard_project/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = "flashcard_project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv('AWS_FLASH_DB_NAME'),
+        "USER": os.getenv('AWS_FLASH_DB_USER'),
+        "PASSWORD": os.getenv('AWS_FLASH_DB_PASSWORD'),
+        "HOST": os.getenv('AWS_FLASH_DB_ENDPOINT'),
+        "PORT": "5432",
     }
 }
 
