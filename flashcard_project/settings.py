4628cd2af5ab4cd64c7cd5465cb09cbc4cd38dd1
Arnau Giribet <arnaugiribetmarch@gmail.com>
2024-12-12 15:23:14 +0100

    modified settings

--- flashcard_project/settings.py
+++ flashcard_project/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = "flashcard_project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": os.getenv('AWS_FLASH_DB_NAME'),
         "USER": os.getenv('AWS_FLASH_DB_USER'),
         "PASSWORD": os.getenv('AWS_FLASH_DB_PASSWORD'),
