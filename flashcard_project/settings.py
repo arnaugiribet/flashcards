bad72094a0267a3f56f4d0f28e5e98d69d716f48
Arnau Giribet <arnaugiribetmarch@gmail.com>
2025-01-04 02:05:17 +0100

    fix_secret_key_env_var_name

--- flashcard_project/settings.py
+++ flashcard_project/settings.py
@@ -28,7 +28,7 @@ sys.path.append(str(BASE_DIR / 'src' / 'backend'))
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
